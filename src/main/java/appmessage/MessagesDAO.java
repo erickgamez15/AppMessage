c50394f389175f46636312e44865f27c33f65899
erickgamez15 <erickgamezsanchez@gmail.com>
2022-08-20 01:09:22 -0500

    We can generate messages

--- src/main/java/appmessage/MessagesDAO.java
+++ src/main/java/appmessage/MessagesDAO.java
@@ -1,9 +1,30 @@
 package appmessage;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MessagesDAO {
     //DAO = Data Acces Object
     public static void generateMessage(Messages message){
-
+        DataBaseConnection connection = new DataBaseConnection();
+        try(Connection cn = connection.getConnection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO `messages` (`message`, `message_author`) " +
+                        "VALUES (?, ?)";
+                ps = cn.prepareStatement(query);
+                ps.setString(1, message.getMessage());
+                ps.setString(2, message.getMessageAuthor());
+                ps.executeUpdate();
+                System.out.println("El mensaje se ha creado correctamente\n");
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }catch (SQLException exception){
+            JOptionPane.showMessageDialog(null,"ERROR!\n");
+        }
     }
 
     public static void  readMessages(){
--- src/main/java/appmessage/MessagesService.java
+++ src/main/java/appmessage/MessagesService.java
@@ -1,8 +1,18 @@
 package appmessage;
 
-public class MessagesService {
-    public static void generateMessage(){
+import java.util.Scanner;
 
+public class MessagesService {
+    static Scanner sc = new Scanner(System.in);
+    public static void generateMessage(){
+        System.out.println("Escribe un mensaje");
+        String message = sc.nextLine();
+        System.out.println("¿Quién escribió el mensaje?");
+        String author = sc.nextLine();
+        Messages messages = new Messages();
+        messages.setMessage(message);
+        messages.setMessageAuthor(author);
+        MessagesDAO.generateMessage(messages);
     }
 
     public static void viewMessage(){
